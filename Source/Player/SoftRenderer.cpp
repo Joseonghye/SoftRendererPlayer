6001dd242bfddb77472d69dd932470dbbdd0b4df
JSH <31675885+Joseonghye@users.noreply.github.com>
2019-09-30 15:53:18 +0900

    190930 Draw Line

--- Source/Player/SoftRenderer.cpp
+++ Source/Player/SoftRenderer.cpp
@@ -31,8 +31,11 @@ void SoftRenderer::Update()
 
 		//RenderCode
 
-
 		//Grid Line
+		LinearColor Red(1.0f, 0.0f, 0.0f);
+		RSI->DrawVerticalLine(0, Red);
+		RSI->DrawHorizontalLine(0, Red);
+
 		ScreenPoint screenSize = DisplaySetting::Inst().GetSize();
 		int halfX = Math::FloorToInt(((float)screenSize.X - 1.f)*0.5f);
 		int halfY = Math::FloorToInt(((float)screenSize.Y - 1.f)*0.5f);
@@ -40,11 +43,11 @@ void SoftRenderer::Update()
 		static int GridSize = 10;
 		LinearColor color(0.5f, 0.5f, 0.5f);
 
-	/*	for (int x = 10; x <halfX; x += GridSize)
+		for (int x = 10; x <halfX; x += GridSize)
 		{
 				RSI->DrawVerticalLine(x, color);
 				RSI->DrawVerticalLine(-x, color);
-		}*/
+		}
 		for (int y = 10; y < halfY; y += GridSize)
 		{
 			RSI->DrawHorizontalLine(y, color);
@@ -69,11 +72,18 @@ void SoftRenderer::Update()
 		i[4] = 1;
 		i[5] = 3;
 
-		RSI->SetVertexBuffer(v);
-		RSI->SetIndexBuffer(i);
-		RSI->DrawPrimitive(4, 6);
+	//	RSI->SetVertexBuffer(v);
+	//	RSI->SetIndexBuffer(i);
+	//	RSI->DrawPrimitive(4, 6);
 
+		/*RSI->DrawLine(Vector2(50.0f, 50.0f), Vector2(100.f, 100.f), LinearColor(0.f, 1.f, 1.f));
+		RSI->DrawLine(Vector2(30.f, 30.f), Vector2(100.f, 300.f), LinearColor(0.f, 0.f, 1.f));
+
+		RSI->DrawLine(Vector2(-50.0f, 50.0f), Vector2(-100.f, 100.f), LinearColor(0.f, 1.f, 1.f));
+		RSI->DrawLine(Vector2(-30.f, 30.f), Vector2(-100.f, 300.f), LinearColor(0.f, 0.f, 1.f));*/
 		
+		RSI->DrawLine(Vector2(50.0f, -50.0f), Vector2(100.f, -100.f), LinearColor(0.f, 1.f, 1.f));
+		RSI->DrawLine(Vector2(30.f, -30.f), Vector2(100.f, -300.f), LinearColor(0.f, 0.f, 1.f));
 
 
 		RSI->EndFrame();
