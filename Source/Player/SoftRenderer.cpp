268a03485fc7f2642168cc074cc3dbc5037fd91d
JSH <31675885+Joseonghye@users.noreply.github.com>
2019-10-07 15:55:23 +0900

    Add files via upload

--- Source/Player/SoftRenderer.cpp
+++ Source/Player/SoftRenderer.cpp
@@ -54,7 +54,7 @@ void SoftRenderer::Update()
 			RSI->DrawHorizontalLine(-y, color);
 		}
 
-		VertexData v[4];
+		/*VertexData v[4];
 		v[0].pos = Vector2(100.0f, 100.0f);
 		v[0].color = LinearColor(1.0f, 0.0f, 0.0f);
 		v[1].pos = Vector2(-100.0f, -100.0f);
@@ -70,24 +70,81 @@ void SoftRenderer::Update()
 		i[2] = 2;
 		i[3] = 0;
 		i[4] = 1;
-		i[5] = 3;
+		i[5] = 3;*/
 
 	//	RSI->SetVertexBuffer(v);
 	//	RSI->SetIndexBuffer(i);
 	//	RSI->DrawPrimitive(4, 6);
 
-		/*RSI->DrawLine(Vector2(50.0f, 50.0f), Vector2(100.f, 100.f), LinearColor(0.f, 1.f, 1.f));
-		RSI->DrawLine(Vector2(30.f, 30.f), Vector2(100.f, 300.f), LinearColor(0.f, 0.f, 1.f));
+		  // Define Mesh Data
+		VertexData v[9];
+		v[0].pos = Vector2(-150.0f, -100.0f);
+		v[0].color = LinearColor(1.0f, 0.0f, 0.0f);
+		v[1].pos = Vector2(-200.0f, 100.0f);
+		v[1].color = LinearColor(0.0f, 1.0f, 0.0f);
+		v[2].pos = Vector2(-100.0f, 100.0f);
+		v[2].color = LinearColor(0.0f, 0.0f, 1.0f);
 
-		RSI->DrawLine(Vector2(-50.0f, 50.0f), Vector2(-100.f, 100.f), LinearColor(0.f, 1.f, 1.f));
-		RSI->DrawLine(Vector2(-30.f, 30.f), Vector2(-100.f, 300.f), LinearColor(0.f, 0.f, 1.f));*/
-		
-		RSI->DrawLine(Vector2(50.0f, -50.0f), Vector2(100.f, -100.f), LinearColor(0.f, 1.f, 1.f));
-		RSI->DrawLine(Vector2(30.f, -30.f), Vector2(100.f, -300.f), LinearColor(0.f, 0.f, 1.f));
-		
-		RSI->DrawLine(Vector2(-50.0f, -50.0f), Vector2(-100.f, -100.f), LinearColor(0.f, 1.f, 1.f));
-		RSI->DrawLine(Vector2(-30.f, -30.f), Vector2(-100.f, -300.f), LinearColor(0.f, 0.f, 1.f));
+		v[3].pos = Vector2(150.0f, 100.0f);
+		v[3].color = LinearColor(1.0f, 0.0f, 0.0f);
+		v[4].pos = Vector2(100.0f, -100.0f);
+		v[4].color = LinearColor(0.0f, 1.0f, 0.0f);
+		v[5].pos = Vector2(200.0f, -100.0f);
+		v[5].color = LinearColor(0.0f, 0.0f, 1.0f);
 
+		v[6].pos = Vector2(50.0f, 100.0f);
+		v[6].color = LinearColor(1.0f, 0.0f, 0.0f);
+		v[7].pos = Vector2(-50.0f, 0.0f);
+		v[7].color = LinearColor(0.0f, 1.0f, 0.0f);
+		v[8].pos = Vector2(0.0f, -100.0f);
+		v[8].color = LinearColor(0.0f, 0.0f, 1.0f);
+
+		int i[9];
+		i[0] = 0;
+		i[1] = 1;
+		i[2] = 2;
+		i[3] = 3;
+		i[4] = 4;
+		i[5] = 5;
+		i[6] = 6;
+		i[7] = 7;
+		i[8] = 8;
+
+		// Draw Call
+		RSI->SetVertexBuffer(v);
+		RSI->SetIndexBuffer(i);
+		RSI->DrawPrimitive(9, 9);
+
+
+
+	//	RSI->DrawLine(Vector2(30., 30.), Vector2(100.f, 100.f), LinearColor(0.f, 1.f, 1.f));
+	//	RSI->DrawLine(Vector2(30.f, 30.f), Vector2(100.f, 300.f), LinearColor(0.f, 0.f, 1.f));
+	//	RSI->DrawLine(Vector2(30.f, 30.f), Vector2(300.f, 100.f), LinearColor(0.f, 0.f, 1.f));
+
+	//	RSI->DrawLine(Vector2(-50.0f, 50.0f), Vector2(-100.f, 100.f), LinearColor(0.f, 1.f, 1.f));
+	//	RSI->DrawLine(Vector2(-30.f, 30.f), Vector2(-100.f, 300.f), LinearColor(0.f, 0.f, 1.f));
+		
+	//	RSI->DrawLine(Vector2(50.0f, -50.0f), Vector2(100.f, -100.f), LinearColor(0.f, 1.f, 1.f));
+	//	RSI->DrawLine(Vector2(30.f, -30.f), Vector2(100.f, -300.f), LinearColor(0.f, 0.f, 1.f));
+
+	//	RSI->DrawLine(Vector2(-50.0f, -50.0f), Vector2(-100.0f, -100.0f), LinearColor(0.f, 1.f, 1.f));
+		//RSI->DrawLine(Vector2(-30.f, -30.f), Vector2(-100.0f, -300.0f), LinearColor(0.f, 0.f, 1.f));
+
+		//// 1사분면
+		//RSI->DrawLine(Vector2(30.f, 30.f), Vector2(100.f, 200.f), LinearColor(0.f, 0.f, 0.f));
+		//RSI->DrawLine(Vector2(30.f, 30.f), Vector2(200.f, 100.f), LinearColor(0.f, 0.f, 0.f));
+
+		//// 2사분면
+		//RSI->DrawLine(Vector2(-30.f, 30.f), Vector2(-100.f, 200.f), LinearColor(0.f, 0.f, 0.f));
+		//RSI->DrawLine(Vector2(-30.f, 30.f), Vector2(-200.f, 100.f), LinearColor(0.f, 0.f, 0.f));
+
+		//// 3사분면
+		//RSI->DrawLine(Vector2(-30.f, -30.f), Vector2(-100.f, -200.f), LinearColor(0.f, 0.f, 0.f));
+		//RSI->DrawLine(Vector2(-30.f, -30.f), Vector2(-200.f, -100.f), LinearColor(0.f, 0.f, 0.f));
+
+		//// 4사분면
+		//RSI->DrawLine(Vector2(30.f, -30.f), Vector2(100.f, -200.f), LinearColor(0.f, 0.f, 0.f));
+		//RSI->DrawLine(Vector2(30.f, -30.f), Vector2(200.f, -100.f), LinearColor(0.f, 0.f, 0.f));
 
 		RSI->EndFrame();
 	}
